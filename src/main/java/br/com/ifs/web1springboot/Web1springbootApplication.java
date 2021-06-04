82301abf2b4231700643e92a5ab6cb590d8c530e
GamaGustavo <gustavo98ag@gmail.com>
2021-06-04 15:07:27 -0300

    Initial commit

---
+++ src/main/java/br/com/ifs/web1springboot/Web1springbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifs.web1springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web1springbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web1springbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ifs/web1springboot/Web1springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifs.web1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web1springbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
